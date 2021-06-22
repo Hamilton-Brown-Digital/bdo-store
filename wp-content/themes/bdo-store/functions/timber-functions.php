1cbc0ff4fc5c40029038a5ffa7aa0a3bdef2df36
mattalmanac <matt@almanac.digital>
2021-06-22 14:57:48 +0100

    Added ACF and markup for Header with Utility nav and Hero and Footer

--- wp-content/themes/bdo-store/functions/timber-functions.php
+++ wp-content/themes/bdo-store/functions/timber-functions.php
@@ -14,12 +14,18 @@ class CustomTimber extends TimberSite
 
     if (has_nav_menu('navigation_primary')) {
       $context['navigation_primary'] = new TimberMenu('navigation_primary', array(
-        'depth' => 2
+        'depth' => 1
       ));
     }
 
-    if (has_nav_menu('navigation_footer')) {
-      $context['navigation_footer'] = new TimberMenu('navigation_footer');
+    if (has_nav_menu('navigation_utility')) {
+      $context['navigation_utility'] = new TimberMenu('navigation_utility', array(
+        'depth' => 1
+      ));
+    }
+
+    if (has_nav_menu('navigation_legal')) {
+      $context['navigation_legal'] = new TimberMenu('navigation_legal');
     }
 
     $context['site'] = $this;
