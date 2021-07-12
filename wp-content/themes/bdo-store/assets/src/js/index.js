612008800ea96518f85bf7523d33493eee533459
mattalmanac <matt@almanac.digital>
2021-07-12 15:15:24 +0100

    Added back to top

--- wp-content/themes/bdo-store/assets/src/js/index.js
+++ wp-content/themes/bdo-store/assets/src/js/index.js
@@ -1,6 +1,7 @@
 import Header from './classes/Header.js';
 import HeroCarousel from './classes/HeroCarousel.js';
 import Accordion from './classes/Accordion.js';
+import BackToTop from './classes/BackToTop.js';
 import Video from './classes/Video.js';
 import Init from './classes/Init';
 
@@ -21,6 +22,11 @@ const siteFunctions = {
   accordion__ready() {
     const accordion = new Accordion();
     accordion.init();
+  },
+
+  backToTop__ready() {
+    const backToTop = new BackToTop();
+    backToTop.init();
   },
 
   video__ready() {
