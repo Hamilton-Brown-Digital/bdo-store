1cacd11f8dde6a70150744d63d9e4dee36eb5b90
mattalmanac <matt@almanac.digital>
2021-07-21 09:39:01 +0100

    Trying to fic 404 again

--- wp-content/themes/bdo-store/404.php
+++ wp-content/themes/bdo-store/404.php
@@ -1,12 +1,4 @@
-<?
-/**
- * The template for displaying 404 pages (not found).
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package wppt
- */
-
+<?php
 
 if ( ! class_exists( 'Timber' ) ) {
 	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wordpress/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
